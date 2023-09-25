22bfb255d8eec3266b3f1dd94881fdb4f5fe4f0f
RisheekNayak <risheek.nayak@gmail.com>
2023-09-25 21:20:23 +0530

    Linked the images with the websites.

--- public/javascrips.js
+++ public/javascrips.js
@@ -1,3 +1,4 @@
+// Making navbar active on current page
 document.addEventListener('DOMContentLoaded', function() {
     const url = this.documentURI;
     const links = document.querySelectorAll('.onClickActive');
